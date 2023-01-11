0cbb164a8456d42708871caccb76b214cf2dc747
Garfield App <garfieldapp@outlook.com>
2023-01-11 15:57:57 +0100

    proxy cors change

--- app.js
+++ app.js
@@ -260,9 +260,9 @@ const fetchUrl = async () =>
 {
   const websiteData = await fetch(siteUrl, {
     method: "GET",
-    headers: {
+    /*headers: {
       "x-cors-grida-api-key": "77a0175b-4435-49b0-ad18-52d2dea5a548"
-    }
+    }*/
   });
   const textData = await websiteData.text();
  return textData;
