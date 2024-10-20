d44781eab2c9c31e859fe9857021f18f6768157e
Bart <dirkjanapp@outlook.com>
2024-10-20 10:24:12 +0200

    service worker caching same as garfieldapp

--- serviceworker.js
+++ serviceworker.js
@@ -1,65 +1,16 @@
+const CACHE = "pwabuilder-offline";
 
-const OFFLINE_VERSION = 2;
-const CACHE_NAME = 'offline';
-const OFFLINE_URL = 
-[
-  "./index.html",
-  "./dirk-jan-tekst.svg",
-  "./mail.webp",
-  "./pwa-pass-3.svg",
-  "./swiped-events.min.js",    
-  "./app.js",
-  "./main.css",
-  "./serviceworker.js",
-  "./mstile-150x150.webp",
-  "./favicon.ico",
-  "./favicon-32x32.webp",
-  "./favicon-16x16.webp",
-  "./apple-touch-icon.webp",
-  "./android-chrome-192x192.png",
-  "./android-chrome-256x256.png",
-  "./android-chrome-512x512.png",
-  "./dirkjantransparent.webp"
-];
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-self.addEventListener('install', (event) => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
-  })());
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-   
-    if ('navigationPreload' in self.registration) {
-      await self.registration.navigationPreload.enable();
-    }
-  })());
-
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
- 
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResponse = await event.preloadResponse;
-        if (preloadResponse) {
-          return preloadResponse;
-        }
-
-        const networkResponse = await fetch(event.request);
-        return networkResponse;
-      } catch (error) {
-       
-        console.log('Fetch failed; returning offline page instead.', error);
-
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
-        return cachedResponse;
-      }
-    })());
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
   }
 });
+
+workbox.routing.registerRoute(
+  new RegExp('.*'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: CACHE
+  })
+);
