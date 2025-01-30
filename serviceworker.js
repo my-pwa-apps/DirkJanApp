a34bd8061d1f768bcad5e195410c40b81cafc607
Bart <dirkjanapp@outlook.com>
2025-01-30 10:53:02 +0100

    serviceworker.js

--- serviceworker.js
+++ serviceworker.js
@@ -14,3 +14,45 @@ workbox.routing.registerRoute(
     cacheName: CACHE
   })
 );
+
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    const cache = await caches.open(CACHE);
+    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
+  })());
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+   
+    if ('navigationPreload' in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  })());
+
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+ 
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResponse = await event.preloadResponse;
+        if (preloadResponse) {
+          return preloadResponse;
+        }
+
+        const networkResponse = await fetch(event.request);
+        return networkResponse;
+      } catch (error) {
+       
+        console.log('Fetch failed; returning offline page instead.', error);
+
+        const cache = await caches.open(CACHE);
+        const cachedResponse = await cache.match(OFFLINE_URL);
+        return cachedResponse;
+      }
+    })());
+  }
+});
