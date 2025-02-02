2970226380fd462e91793c3c6ba95e6187ab8824
Bart <dirkjanapp@outlook.com>
2025-02-02 12:02:14 +0100

    service worker restore

--- serviceworker.js
+++ serviceworker.js
@@ -1,23 +1,30 @@
-const CACHE = "pwabuilder-offline";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
-});
-
-workbox.routing.registerRoute(
-  new RegExp('.*'),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE
-  })
-);
+const OFFLINE_VERSION = 2;
+const CACHE_NAME = 'offline';
+const OFFLINE_URL = 
+[
+  "./index.html",
+  "./dirk-jan-tekst.svg",
+  "./mail.webp",
+  "./pwa-pass-3.svg",
+  "./swiped-events.min.js",    
+  "./app.js",
+  "./main.css",
+  "./serviceworker.js",
+  "./mstile-150x150.webp",
+  "./favicon.ico",
+  "./favicon-32x32.webp",
+  "./favicon-16x16.webp",
+  "./apple-touch-icon.webp",
+  "./android-chrome-192x192.png",
+  "./android-chrome-256x256.png",
+  "./android-chrome-512x512.png",
+  "./dirkjantransparent.webp"
+];
 
 self.addEventListener('install', (event) => {
   event.waitUntil((async () => {
-    const cache = await caches.open(CACHE);
+    const cache = await caches.open(CACHE_NAME);
     await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
   })());
 });
@@ -49,7 +56,7 @@ self.addEventListener('fetch', (event) => {
        
         console.log('Fetch failed; returning offline page instead.', error);
 
-        const cache = await caches.open(CACHE);
+        const cache = await caches.open(CACHE_NAME);
         const cachedResponse = await cache.match(OFFLINE_URL);
         return cachedResponse;
       }
